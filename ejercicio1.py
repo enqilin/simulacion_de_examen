7372a764ade669e31939b2ae948b83648f5045d4
enqilin <114181724+enqilin@users.noreply.github.com>
2022-12-18 22:50:20 +0000

    ejercicio1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,23 +1,18 @@
 from ast import main
 
 def minion_game(palabras):
-    vowels = 'AEIOU'
-    kevin_score = 0
-    stuart_score = 0
+    sub_palabras = {}
     for i in range(len(palabras)):
-        if palabras[i] in vowels:
-            kevin_score += (len(palabras)-i)
-        else:
-            stuart_score += (len(palabras)-i)
-            if kevin_score < stuart_score:
-                print('Stuart', stuart_score)
-            elif kevin_score > stuart_score:
-                print ('Kevin', kevin_score)
-            else:
-                print('Draw')
-
-
-
-
-if __name__ == "__main__":
-    minion_game()
+       if palabras[i] in 'AEIOU': 
+            for j in range(i+1, len(palabras)+1):
+                sub_palabras[palabras[i:j]] = sub_palabras.get(palabras[i:j], 0) + 1
+       else:
+            for j in range(i+1, len(palabras)+1):
+                sub_palabras[palabras[i:j]] = sub_palabras.get(palabras[i:j], 0) + 1
+    if sub_palabras.keys() in 'AEIOU':
+        print('Kevin', sub_palabras.items())
+    else:
+        print('Stuart', sub_palabras.items())
+minion_game(input())
+if __name__ == '__main__':
+    main()
